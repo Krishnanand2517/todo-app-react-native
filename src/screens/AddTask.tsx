46342f1df815c703d1ea80ffe5cd3f9249d4adbf
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-02-14 19:46:20 +0530

    Add date time picker to AddTask and EditTask screens

--- src/screens/AddTask.tsx
+++ src/screens/AddTask.tsx
@@ -8,6 +8,8 @@ import {
   TextInput,
 } from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import DatePicker from 'react-native-date-picker';
+import Icon from 'react-native-vector-icons/Feather';
 
 import {RootStackParamList} from '../App';
 
@@ -15,6 +17,14 @@ type AddTaskProps = NativeStackScreenProps<RootStackParamList, 'AddTask'>;
 
 interface DoneButtonProps {
   onDoneButtonPressed: () => void;
+}
+
+interface AddDateButtonProps {
+  onAddDateButtonPressed: () => void;
+}
+
+interface AddTimeButtonProps {
+  onAddTimeButtonPressed: () => void;
 }
 
 const DoneButton = ({
@@ -25,15 +35,47 @@ const DoneButton = ({
       style={styles.button}
       onPress={onDoneButtonPressed}
       activeOpacity={0.7}>
-      <Text style={styles.buttonText}>DONE</Text>
+      <Icon name="check" size={30} color="#FFF" />
+    </TouchableOpacity>
+  );
+};
+
+const AddDateButton = ({
+  onAddDateButtonPressed,
+}: AddDateButtonProps): React.JSX.Element => {
+  return (
+    <TouchableOpacity
+      style={styles.dateTimeButton}
+      onPress={onAddDateButtonPressed}
+      activeOpacity={0.7}>
+      <Text style={styles.dateTimeButtonText}>Add Date</Text>
+    </TouchableOpacity>
+  );
+};
+
+const AddTimeButton = ({
+  onAddTimeButtonPressed,
+}: AddTimeButtonProps): React.JSX.Element => {
+  return (
+    <TouchableOpacity
+      style={styles.dateTimeButton}
+      onPress={onAddTimeButtonPressed}
+      activeOpacity={0.7}>
+      <Text style={styles.dateTimeButtonText}>Add Time</Text>
     </TouchableOpacity>
   );
 };
 
 const AddTask = ({navigation}: AddTaskProps): React.JSX.Element => {
   const [taskContent, setTaskContent] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [dateModalOpen, setDateModalOpen] = useState(false);
+  const [timeModalOpen, setTimeModalOpen] = useState(false);
 
   const onDoneButtonPressed = () => navigation.navigate('Home');
+  const onAddDateButtonPressed = () => setDateModalOpen(true);
+  const onAddTimeButtonPressed = () => setTimeModalOpen(true);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -48,6 +90,68 @@ const AddTask = ({navigation}: AddTaskProps): React.JSX.Element => {
           onChangeText={setTaskContent}
         />
       </View>
+      <View style={styles.dateTimeButtonsWrapper}>
+        <View style={styles.dateTimeSection}>
+          <AddDateButton onAddDateButtonPressed={onAddDateButtonPressed} />
+          {date && (
+            <View style={styles.dateTimeTextWrapper}>
+              <Text style={styles.dateTimeText}>{date}</Text>
+            </View>
+          )}
+        </View>
+
+        <View style={styles.dateTimeSection}>
+          <AddTimeButton onAddTimeButtonPressed={onAddTimeButtonPressed} />
+          {time && (
+            <View style={styles.dateTimeTextWrapper}>
+              <Text style={styles.dateTimeText}>{time}</Text>
+            </View>
+          )}
+        </View>
+      </View>
+
+      <DatePicker
+        modal
+        mode="date"
+        open={dateModalOpen}
+        date={new Date()}
+        onConfirm={selectedDate => {
+          setDateModalOpen(false);
+          setDate(
+            selectedDate
+              .toLocaleDateString('en-IN', {
+                day: '2-digit',
+                month: 'short',
+                year: 'numeric',
+              })
+              .split('-')
+              .join(' '),
+          );
+        }}
+        onCancel={() => {
+          setDateModalOpen(false);
+        }}
+      />
+
+      <DatePicker
+        modal
+        mode="time"
+        open={timeModalOpen}
+        date={new Date()}
+        onConfirm={selectedDate => {
+          setTimeModalOpen(false);
+          setTime(
+            selectedDate.toLocaleTimeString('en-IN', {
+              hour: 'numeric',
+              minute: 'numeric',
+              hour12: true,
+            }),
+          );
+        }}
+        onCancel={() => {
+          setTimeModalOpen(false);
+        }}
+      />
     </SafeAreaView>
   );
 };
@@ -73,12 +177,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#33B249',
     paddingHorizontal: 20,
     paddingVertical: 8,
-    borderRadius: 4,
+    borderRadius: 18,
     elevation: 5,
-  },
-  buttonText: {
-    fontSize: 18,
-    color: '#FFFFFF',
   },
   taskInputWrapper: {
     marginTop: 36,
@@ -92,6 +192,41 @@ const styles = StyleSheet.create({
     paddingHorizontal: 12,
     paddingVertical: 14,
     fontSize: 16,
+    color: '#2B2D42',
+  },
+  dateTimeButtonsWrapper: {
+    marginTop: 24,
+    rowGap: 20,
+  },
+  dateTimeButton: {
+    backgroundColor: '#0A79DF',
+    paddingHorizontal: 20,
+    paddingVertical: 8,
+    borderRadius: 8,
+    elevation: 5,
+  },
+  dateTimeButtonText: {
+    fontSize: 16,
+    color: '#FFFFFF',
+    fontWeight: '600',
+    textTransform: 'uppercase',
+  },
+  dateTimeSection: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  dateTimeTextWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#0A79DF',
+    borderRadius: 8,
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+  },
+  dateTimeText: {
+    fontSize: 14,
     color: '#2B2D42',
   },
 });
